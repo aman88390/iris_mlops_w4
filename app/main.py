6c5428fe1bd6882a13a65b13d64ce05312129152
Aman Yadav <aman9005156678@gmail.com>
2025-11-02 09:32:56 +0000

    commited

--- app/main.py
+++ app/main.py
@@ -13,3 +13,35 @@ def root():
 def predict(features: list):
     prediction = model.predict([features])
     return {"prediction": int(prediction[0])}
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+app = FastAPI()
+
+model = pickle.load(open("app/iris_model.pkl", "rb"))
+
+class IrisFeatures(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@app.get("/")
+def root():
+    return {"message": "Iris API running on Kubernetes ✅"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def predict(features: IrisFeatures):
+    data = [[
+        features.sepal_length,
+        features.sepal_width,
+        features.petal_length,
+        features.petal_width
+    ]]
+    prediction = model.predict(data)
+    return {"prediction": int(prediction[0])}
